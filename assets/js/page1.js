ee596c23767de0818f0b4ad1a4bd210061c16354
AlmiraChinkebayeva <naimanka011@gmail.com>
2021-05-21 00:47:32 +0500

    dishes added in menu

---
+++ assets/js/page1.js
@@ -0,0 +1,142 @@
+var foods = [
+        {
+            category: 'Starter',
+            dishes: [
+                {
+                    id: 1,
+                    name: 'Chicken Wings',
+                    description: 'Chicken Wings',
+                    vegetarian: false,
+                    cost: 12
+                },
+                {
+                    id: 2,
+                    name: 'Broccoli',
+                    description: 'Boiled Broccoli',
+                    vegetarian: true,
+                    cost: 5
+                },
+                {
+                    id: 3,
+                    name: 'Lollipops',
+                    description: 'Lollipops',
+                    vegetarian: true,
+                    cost: 2
+                },
+                {
+                    id: 4,
+                    name: 'Bread',
+                    description: 'Bread',
+                    vegetarian: false,
+                    cost: 6
+                }
+            ]
+        },
+        {
+            category: 'Main',
+            dishes: [
+                {
+                    id: 5,
+                    name: 'Grill Burger',
+                    description: 'Double Stake Grill Burger',
+                    vegetarian: false,
+                    cost: 25
+                },
+                {
+                    id: 6,
+                    name: 'Beef stake',
+                    description: 'Beef stake',
+                    vegetarian: false,
+                    cost: 30
+                },
+                {
+                    id: 7,
+                    name: 'Egg Fried Rice',
+                    description: 'Egg Fried Rice',
+                    vegetarian: false,
+                    cost: 15
+                },
+                {
+                    id: 8,
+                    name: 'Orecchiette Pasta with Broccoli Sauce',
+                    description: 'a simple vegetarian pasta recipe with a flavorful Tuscan-style Broccoli Sauce! An easy and delicious vegetarian dinner recipe that highlights beautiful broccoli!',
+                    vegetarian: true,
+                    cost: 20
+                },
+            ]
+        },
+        {
+            category: 'Deserts',
+            dishes: [
+                {
+                    id: 9,
+                    name: 'Ice Cream',
+                    description: 'Vanila Ice Cream',
+                    vegetarian: false,
+                    cost: 10
+                },
+                {
+                    id: 10,
+                    name: 'russian salad',
+                    description: 'russian salad',
+                    vegetarian: true,
+                    cost: 15
+                },
+                {
+                    id: 11,
+                    name: 'Molten Lava',
+                    description: 'Molten Lava Served With Hot Chocolate',
+                    vegetarian: false,
+                    cost: 30
+                }
+            ]
+        },
+        {
+            category: 'Drinks',
+            dishes: [
+                {
+                    id: 12,
+                    name: 'Coca Cola',
+                    description: 'Vanila Ice Cream',
+                    vegetarian: false,
+                    cost: 5
+                },
+                {
+                    id: 13,
+                    name: 'Sprite',
+                    description: 'russian salad',
+                    vegetarian: false,
+                    cost: 5
+                },
+                {
+                    id: 14,
+                    name: 'Mint Margarita',
+                    description: 'Mint Margarita',
+                    vegetarian: true,
+                    cost: 10
+                }
+            ]
+        }
+    ],
+    populateRecipes = function () {
+        var html = '';
+        for (var i = 0; i < foods.length; i++) {
+
+            html += '<thead>';
+            html += '<tr>';
+            html += '<th>';
+            html += '<h2 class="sub-heading">';
+            html += foods[i].category;
+            html += '</h2>';
+            html += '</th>';
+            html += '<th><h2 class="sub-heading">Quantity</h2></th>';
+            html += '</tr>';
+            html += '</thead>';
+        }
+
+        var table = document.getElementById('menu-table');
+        table.innerHTML = html;
+    };
+
+
+populateRecipes();
